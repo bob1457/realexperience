601429a65875368e6c4aedf79da23bb1f5ba16c4
Bob Yuan <bob.yuan@yahoo.com>
2021-05-07 16:04:16 -0700

    App structure adjustment

--- projects/realportal/src/app/app.module.ts
+++ projects/realportal/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { RouteReuseStrategy } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  entryComponents: [],
   imports: [
     BrowserModule,
     AppRoutingModule,
     IonicModule.forRoot()
   ],
-  providers: [],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
